49ebefdcafd7513e9352623f1f33d5e5a4afd585
Digital-Grinnell <digital@grinnell.edu>
2023-09-13 08:19:28 -0500

    Updated after HADC

--- script.sh
+++ script.sh
@@ -1,22 +1,10 @@
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_1997.0006.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_1997.0006.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted_1024_768 Paintings/WCA_1998.0178.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted_1024_768 Paintings/WCA_1998.0178a.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted_1024_768 Paintings/WCA_1998.0178b.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted_1024_768 Paintings/WCA_1998.0178b.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted_1024_768 Paintings/WCA_1998.0178c.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted_1024_768 Paintings/WCA_1998.0178c.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0231.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0231.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0232.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0232.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0233.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0233.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0234.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0234.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0235.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0235.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0236.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0236.002.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0237.001.jpg" /Volumes/DGIngest/HADC-250/.
-cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0237.002.jpg" /Volumes/DGIngest/HADC-250/.
+# Do NOT 'source' this script, copy and paste it line-for-line.
+
+cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_1979.013.001.jpg" /Volumes/DGIngest/HADC-250/.
+cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_1981.0009.001.jpg" /Volumes/DGIngest/HADC-250/.
+cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 768 Flagss/WCA_1998.0066.001.jpg" /Volumes/DGIngest/HADC-250/.
+cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 768 Flagss/WCA_1998.0082.001.jpg" /Volumes/DGIngest/HADC-250/. 
+cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 768 Flagss/WCA_1998.0083.001.jpg" /Volumes/DGIngest/HADC-250/. 
+cp -f "/Volumes/HADC_Repository/WordPress Images/Converted 1024 by 768 Metals/WCA_2007.0325.001.jpg" /Volumes/DGIngest/HADC-250/.
+
+
